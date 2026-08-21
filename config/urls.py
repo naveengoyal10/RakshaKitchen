8d13aa4734abc1e8a59f45d7a34ddb60b233172c
Naveen Goyal <naveengoyal10@gmail.com>
2026-08-21 22:35:40 +0530

    google file

--- config/urls.py
+++ config/urls.py
@@ -2,27 +2,37 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from django.http import HttpResponse
 from django.urls import include, path
-from django.views.generic import TemplateView
 
 from kitchen.sitemaps import KitchenSitemap
 
+
+def google_verification(request):
+    return HttpResponse(
+        "google-site-verification: google8e50f1393451d06.html",
+        content_type="text/html",
+    )
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("kitchen.urls")),
+
+    # Google Search Console verification
     path(
         "google8e50f1393451d06.html",
-        TemplateView.as_view(
-            template_name="google8e50f1393451d06.html",
-            content_type="text/html",
-        ),
+        google_verification,
     ),
+
     path(
         "sitemap.xml",
         sitemap,
         {"sitemaps": {"kitchen": KitchenSitemap}},
     ),
+
+    path("", include("kitchen.urls")),
 ]
+
 
 if settings.DEBUG or settings.VERCEL:
     urlpatterns += static(
