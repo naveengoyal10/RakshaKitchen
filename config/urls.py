629e40d5180ed1f15edb5c1568076f8b7febe992
Naveen Goyal <naveengoyal10@gmail.com>
2026-08-21 22:28:45 +0530

    google file

--- config/urls.py
+++ config/urls.py
@@ -3,14 +3,29 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from kitchen.sitemaps import KitchenSitemap
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("kitchen.urls")),
-    path("sitemap.xml", sitemap, {"sitemaps": {"kitchen": KitchenSitemap}}),
+    path(
+        "google8e50f1393451d06.html",
+        TemplateView.as_view(
+            template_name="google8e50f1393451d06.html",
+            content_type="text/html",
+        ),
+    ),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": {"kitchen": KitchenSitemap}},
+    ),
 ]
 
 if settings.DEBUG or settings.VERCEL:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
